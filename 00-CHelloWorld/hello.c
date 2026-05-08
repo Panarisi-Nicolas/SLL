9657f4377940010fb9d753e06ce77b0e8ff93c99
Panarisi-Nicolas <pnicolas@frba.utn.edu.ar>
2026-05-07 23:15:42 -0300

    Agregando Ejecutable con ignore

---
+++ 00-CHelloWorld/hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    // argc = cantidad de argumentos
+    // argv = vector de strings con los argumentos
+
+    printf("Hello, World!\n");
+
+    return 0;
+}
